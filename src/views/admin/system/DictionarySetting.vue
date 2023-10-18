d880c19786cee5f394070d3f64bf7e1d4a9be91e
swipth <13218037380@163.com>
2023-10-18 09:45:04 +0800

    feat: init

---
+++ src/views/admin/system/DictionarySetting.vue
@@ -0,0 +1,20 @@
+<!--
+ * @Author: swipth
+ * @Description:
+-->
+<script lang="ts">
+import {Vue, Options} from 'vue-class-component';
+
+@Options({})
+export default class DictionarySetting extends Vue {
+}
+</script>
+
+<template>
+  <div>DictionarySetting</div>
+
+</template>
+
+<style scoped lang="less">
+
+</style>
---
+++ src/views/admin/system/DictionaryType.vue
@@ -0,0 +1,20 @@
+<!--
+ * @Author: swipth
+ * @Description:
+-->
+<script lang="ts">
+import {Vue, Options} from 'vue-class-component';
+
+@Options({})
+export default class DictionaryType extends Vue {
+}
+</script>
+
+<template>
+  <div>DictionaryType</div>
+
+</template>
+
+<style scoped lang="less">
+
+</style>
