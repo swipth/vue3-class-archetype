5a0c8d9dec8ce6d65bc223634268ff4a64e2b6c9
swipth <13218037380@163.com>
2023-12-15 13:32:14 +0800

    feat: init

--- src/api/interface/example.ts
+++ src/api/interface/example.ts
@@ -1,5 +1,10 @@
-import {AjaxRes} from "@/types/common";
-import {ajaxGet} from "@/api/interface/index";
+import {ajaxGet, ajaxPost} from "./index";
 
-// 查询
-export const getAwardData = (): Promise<AjaxRes> => ajaxGet("/common/v1/admin/award");
+// 参考示例编写http请求
+export const ExampleApi = {
+  // 参数名称简洁详细列出参数分界线为4个
+  receive: (orderId: string) => ajaxGet("/api/mall/outstock", {orderId}),
+  // 以下为超出4个参数显示情况  使用filterParams省略显示 需要做类型控制
+  receive2: (filterParams: {name:string;age:number;sex:number;nation:string}) => ajaxPost("/api/mall/outstock", filterParams)
+}
+
