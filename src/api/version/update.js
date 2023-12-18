1d32cbdfd5319705b9692724e2e3605c892bca46
swipth <13218037380@163.com>
2023-12-18 14:49:31 +0800

    feat: init

--- src/api/version/update.js
+++ src/api/version/update.js
@@ -4,7 +4,7 @@ const { currentVersion } = require("./info");
 const writeVersion = () => {
   // 写入文件
   FStream.writeFile(
-    path.join(__dirname, "../" + process.env.VUE_APP_OUT_PUT_NAME + "/static/version.json"),
+    path.join(__dirname, "../../../" + process.env.VUE_APP_OUT_PUT_NAME + "/static/version.json"),
     JSON.stringify({
       success: true,
       data: { version: currentVersion },
