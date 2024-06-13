59efb7883a2219992f43a527e5a2a9c2e336af71
swipth <13218037380@163.com>
2024-06-13 10:13:08 +0800

    feat: init

--- src/router/guard/index.ts
+++ src/router/guard/index.ts
@@ -1,6 +1,7 @@
 import NProgress from "nprogress"; // Progress 进度条
 import router from "@/router/index";
 import setPageTitle from "@/config/pageTitle";
+import {versionCheck} from "@/api/version";
 
 NProgress.configure({showSpinner: false});
 router.beforeEach((to, from, next: () => void) => {
@@ -11,3 +12,6 @@ router.afterEach((to) => {
   setPageTitle(to.meta)
   NProgress.done(); // 结束Progress
 });
+router.onError(() => {
+  versionCheck()
+})
